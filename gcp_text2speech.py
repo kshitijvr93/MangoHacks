da9f27f3cb68c832214c0e5accf4a54d336f0948
Kshitij Ranganath <kshitijvr93@gmail.com>
2019-02-02 07:17:09 -0500

    minor changes

--- gcp_text2speech.py
+++ gcp_text2speech.py
@@ -4,13 +4,6 @@ import argparse
 import io
 import re
 
-
-
-
-
-
-
-
 def detect_web(path):
     """Detects web annotations given an image."""
     from google.cloud import vision
@@ -38,30 +31,34 @@ text1 = detect_web("C:\\Users\\kshit\\Downloads\\MangoHacks\\images\\Mona_Lisa.j
 
 
 
-from google.cloud import texttospeech
 
-# Instantiates a client
-client = texttospeech.TextToSpeechClient()
 
-# Set the text input to be synthesized
-synthesis_input = texttospeech.types.SynthesisInput(text=text1)
+def text2speech(text1):
+    from google.cloud import texttospeech
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
 
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("neutral")
-voice = texttospeech.types.VoiceSelectionParams(
-    language_code='en-US',
-    ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.types.SynthesisInput(text=text1)
 
-# Select the type of audio file you want returned
-audio_config = texttospeech.types.AudioConfig(
-    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
 
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-response = client.synthesize_speech(synthesis_input, voice, audio_config)
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
 
-# The response's audio_content is binary.
-with open('output.mp3', 'wb') as out:
-    # Write the response to the output file.
-    out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+
+    # The response's audio_content is binary.
+    with open('output.mp3', 'wb') as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+
+text2speech(text1)
